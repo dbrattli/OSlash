1574c4eb0e5746f2eb4ac4fdc0eca420ebcf75bb
Dag Brattli <dag@brattli.net>
2015-03-08 14:57:35 +0100

    Added some docstrings

--- ø/monoid.py
+++ ø/monoid.py
@@ -6,17 +6,28 @@ class Monoid(metaclass=ABCMeta):
     @classmethod
     @abstractmethod
     def mempty(cls) -> "Monoid":
-        """mempty :: m"""
+        """mempty :: m
+
+        Identity of mappend"""
+
         return NotImplemented
 
     @abstractmethod
     def mappend(self, other) -> "Monoid":
-        """mappend :: m -> m -> m"""
+        """mappend :: m -> m -> m
+
+        An associative operation"""
+
         return NotImplemented
 
     @classmethod
     def mconcat(cls, xs) -> "Monoid":
-        """mconcat :: [m] -> m"""
+        """mconcat :: [m] -> m
+
+        Fold a list using the monoid. For most types, the default definition
+        for mconcat will be used, but the function is included in the class
+        definition so that an optimized version can be provided for specific
+        types."""
 
         reducer = lambda a, b: a.mappend(b)
         return reduce(reducer, xs, cls.mempty())
