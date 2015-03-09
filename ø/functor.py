ff418f66e3e606bf3a421b3b2bb60756781becbd
Dag Brattli <dag@brattli.net>
2015-03-09 20:09:18 +0100

    Updated docs

--- ø/functor.py
+++ ø/functor.py
@@ -2,10 +2,21 @@ from abc import ABCMeta, abstractmethod
 
 
 class Functor(metaclass=ABCMeta):
+    """The Functor class is used for types that can be mapped over. Instances
+    of Functor should satisfy the following laws:
+
+    fmap id  ==  id
+    fmap (f . g)  ==  fmap f . fmap g
+
+    The instances of Functor for lists, Maybe and IO satisfy these laws.
+    """
 
     @abstractmethod
     def fmap(self, func) -> "Functor":
-        """
+        """fmap knows how to apply functions to values that are wrapped in a
+        context.
+
+        Keyword arguments:
         :param Functor[A] self: Functor to map
         :param Callable[[A], B] func: Mapper function
         :rtype: Functor[B]
