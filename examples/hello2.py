a37dd0eb0f2f130da1c6114313b504bdc416543a
Dag Brattli <dag@brattli.net>
2015-03-13 08:41:47 +0100

    Added expression tree example

---
+++ examples/hello2.py
@@ -0,0 +1,12 @@
+from oslash import Put, Get, IOAction
+
+main = Put("What is your name?",
+       Get(lambda name:
+       Put("What is your age?",
+       Get(lambda age:
+       Put("Hello " + name + "!",
+       Put("You are " + age + " years old",
+       IOAction(())))))))
+
+if __name__ == "__main__":
+    main()
