cd90dc2afee3b48cf79200eccdf00739019a7112
Dag Brattli <dag@brattli.net>
2015-04-17 07:09:22 +0200

    Refactored list

--- oslash/list.py
+++ oslash/list.py
@@ -1,3 +1,4 @@
+import collections
 from functools import partial
 
 from typing import Any, Callable, Iterator
@@ -15,31 +16,37 @@ class List(Monad, Monoid, Applicative, Functor):
     Wraps an immutable list built from lambda expressions.
     """
 
-    def __init__(self, xs=None, fn=None):
-        if xs and not isinstance(xs, List):
-            # Builds List from iterable using cons only
-            l = List(xs[1:]).cons(xs[0])
-            fn = l._get_value()
+    def __init__(self, lambda_list: Callable[[Callable], Any]=None):
+        """Initialize List."""
+        # Accept building List from a Python iterable
+        if isinstance(lambda_list, collections.Iterable):
+            # Partially build List from iterable using cons only
+            l = List(lambda_list[1:]).cons(lambda_list[0]) if lambda_list else List()
+            lambda_list = l._get_value()
 
-        self._get_value = lambda: fn
+        self._get_value = lambda: lambda_list
 
-    def cons(self, value):
-        xs = self._get_value()
-        return List(fn=lambda fn_: fn_(value, xs))
+    def cons(self, element: Any) -> 'List':
+        """Append element to List."""
+        lambda_list = self._get_value()
+        return List(lambda sel: sel(element, lambda_list))
 
-    def head(self):
-        xs = self._get_value()
-        return xs(lambda x, _: x)
+    def head(self) -> Any:
+        """Retrive first element in List."""
+        lambda_list = self._get_value()
+        return lambda_list(lambda head, _: head)
 
-    def tail(self):
-        xs = self._get_value()
-        return List(fn=xs(lambda _, xs_: xs_))
+    def tail(self) -> 'List':
+        """Return tail of List."""
+        lambda_list = self._get_value()
+        return List(lambda_list(lambda _, tail: tail))
 
-    def null(self):
+    def null(self) -> bool:
+        """Return True if List is emtpty."""
         return not self._get_value()
 
     @classmethod
-    def unit(cls, value):
+    def unit(cls, value: Any):
         """ Wraps a value within the singleton list"""
         return cls().cons(value)
     pure = unit
@@ -63,18 +70,22 @@ class List(Monad, Monoid, Applicative, Functor):
 
     @classmethod
     def empty(cls) -> 'List':
-        """Create an empty list"""
-        return cls([])
+        """Create an empty list."""
+        return cls()
 
     def append(self, other: 'List'):
-        """Append a list to this list"""
+        """Append a list to this list."""
         return List(list(self) + list(other))
 
-    def bind(self, func: Callable[[Any], 'List']) -> 'List':
-        # xs >>= f = concat (map f xs)
-        return List.concat(self.map(func))  # aka flat_map
+    def bind(self, fn: Callable[[Any], 'List']) -> 'List':
+        """Flatten and map the List.
+
+        Haskell: xs >>= f = concat (map f xs)
+        """
+        return List.concat(self.map(fn))
 
     def __iter__(self) -> Iterator:
+        """Return iterator for List."""
         xs = self  # Don't think we can avoid this mutable local
         while True:
             if xs.null():
@@ -90,4 +101,5 @@ class List(Monad, Monoid, Applicative, Functor):
         return str(self)
 
     def __eq__(self, other: 'List') -> bool:
+        """Compare if List is equal to other List."""
         return [x for x in self] == [y for y in other]
