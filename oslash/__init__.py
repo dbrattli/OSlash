7b8a2b72a99fd76d2ec4f997a831b04c857d832e
Dag Brattli <dag@brattli.net>
2015-04-25 10:14:30 +0200

    compose to fn

--- oslash/__init__.py
+++ oslash/__init__.py
@@ -5,10 +5,12 @@ from .either import Right, Left
 from .list import List
 from .ioaction import IO, Put, Get, ReadFile, put_line, get_line, read_file
 from .writer import Writer, MonadWriter
-from .reader import Reader, MonadReader, _
+from .reader import Reader, MonadReader
 from .identity import Identity
 from .state import State
 
 from .monadic import *
 
-from .util import compose
+from .util import fn
+
+_ = Reader
