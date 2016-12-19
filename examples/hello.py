d6f88efee9420fef12ee3d16184c7c65ce8754d3
Dag Brattli <dag@brattli.net>
2016-12-19 16:54:31 +0100

    A better way of writing this example

--- examples/hello.py
+++ examples/hello.py
@@ -1,11 +1,15 @@
 from oslash import put_line, get_line
 
-main = put_line("What is your name?") | (lambda _:
-        get_line() | (lambda name:
-        put_line("What is your age?") | (lambda _:
-        get_line() | (lambda age:
-        put_line("Hello " + name + "!") | (lambda _:
-        put_line("You are " + age + " years old"))))))
+main = (
+	put_line("What is your name?") 
+	| (lambda _: get_line() 
+	| (lambda name: put_line("What is your age?") 
+	| (lambda _: get_line() 
+	| (lambda age: put_line("Hello " + name + "!") 
+	| (lambda _: put_line("You are " + age + " years old")
+	))))))
 
 if __name__ == "__main__":
-    main()
+	#print(main)
+	main(world)
+	
