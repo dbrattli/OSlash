3ed4db2e88a60ece57b9026a96a1c81d479b18e9
Dag Brattli <dag@brattli.net>
2018-01-07 09:35:52 +0100

    Fix example

--- examples/hello.py
+++ examples/hello.py
@@ -1,15 +1,14 @@
 from oslash import put_line, get_line
 
 main = (
-	put_line("What is your name?") 
-	| (lambda _: get_line() 
-	| (lambda name: put_line("What is your age?") 
-	| (lambda _: get_line() 
-	| (lambda age: put_line("Hello " + name + "!") 
+	put_line("What is your name?")
+	| (lambda _: get_line()
+	| (lambda name: put_line("What is your age?")
+	| (lambda _: get_line()
+	| (lambda age: put_line("Hello " + name + "!")
 	| (lambda _: put_line("You are " + age + " years old")
 	))))))
 
 if __name__ == "__main__":
 	#print(main)
-	main(world)
-	
+	main()
