cac7406f4add3370d6af6424162eac61b53efeff
Dag Brattli <dag.brattli@cognite.com>
2020-10-12 22:45:44 +0200

    Add infix version of map

--- setup.py
+++ setup.py
@@ -31,6 +31,7 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     install_requires=['typing_extensions'],
